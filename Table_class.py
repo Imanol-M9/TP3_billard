6888f6e69a14e6a692c67204f56f74421fa6a2e7
Rémi Fortier <e2583316@cmaisonneuve.qc.ca>
2026-04-29 09:59:35 -0400

    J'ai ajouté un class Table, qui servira de hub mathematique pour tout nos calcul de colision. J'ai modifer la class Ball pour y ajouter le rayon de la balle. J'ai retirer Herobrine

---
+++ Table_class.py
@@ -0,0 +1,12 @@
+from Ball_class import Ball
+
+
+class Table:
+    def __init__(self, height, base, balls: list[Ball]):
+        self.height = height
+        self.base = base
+        self.balls = balls
+    
+    def __str__(self):
+        print(f"""
+""")
