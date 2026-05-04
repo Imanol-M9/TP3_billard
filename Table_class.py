3f8c77cb58ab1ec4ac24ae117f2586395f6605f9
Rémi <fortitieremi08@gmail.com>
2026-05-04 10:39:28 -0400

    J'ai commencé les collisions J'ai retirer herobrine

--- Table_class.py
+++ Table_class.py
@@ -1,4 +1,7 @@
 from Ball_class import Ball
+import math
+import csv
+from interface import HAUTEUR, LONGEUR, BORDURE, RAYON
 
 
 class Table:
@@ -7,4 +10,23 @@ class Table:
         self.base = base
         self.balls = balls
     
-    
+    def step_and_write(self, filename="current_sim.csv"):
+        fields = ["Time", "Position_White", "Speed_White"]
+        rows = []
+        time = 0
+
+        with open(filename, "w", newline="") as file:
+            while self.balls[0].collision_with_wall(self.base, self.height):
+                rows.append((time, self.balls[0].position, self.balls[0].speed))
+                self.balls[0].step(0.025)
+                time += 1
+            rows.append((time, self.balls[0].position, self.balls[0].speed))
+            csvwriter = csv.writer(file)
+            csvwriter.writerow(fields)
+            csvwriter.writerows(rows)
+
+
+white = Ball("White", [100,100], 0, 20 )
+table = Table(122, 214, [white])
+
+table.step_and_write()
