c642cb7833902c9c8a4fb6e337a9072e544d762f
Imanol <ptitranol@gmail.com>
2026-05-11 11:06:14 -0400

    corection du merge

--- Ball_class.py
+++ Ball_class.py
@@ -9,7 +9,8 @@ class Ball:
         color: str,
         position: list[float] = [0, 0],
         angle: float = 0,
-        norm: float = 0
+        norm: float = 0,
+        objet_canva=None,
     ):
         self.color = color
         self.position = position
@@ -62,14 +63,18 @@ class Ball:
             coordoner[0] - main.RAYON,
             coordoner[1] - main.RAYON,
         )
+
+
 def collision_dot(Delta_v, n):
     return np.dot(Delta_v, n)
+
 
 def speed_ball(ball, v_rel, n):
     v1 = ball.speef
     if v_rel > 0:
         v1 = ball.speed - v_rel * n
     return v1
+
 
 def speed_p(p, v_rel, n):
     v1 = p.speef
@@ -78,11 +83,7 @@ def speed_p(p, v_rel, n):
     return v1
 
 
-
 white = Ball("White", [100, 100], 1, 1)
 red = Ball("Red", [200, 200], 0, 0)
 
-ensemble_balls = {
-    "white" : white,
-    "red" : red
-}
+ensemble_balls = {"white": white, "red": red}
